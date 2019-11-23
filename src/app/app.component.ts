0e06be6463e14f018ba2efe10f64d597f185d1df
Iceyaruck <aiyaruck.b@gmail.com>
2019-11-24 00:25:11 +0700

    edit top bar and new profile banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularJs-IceYahz';
+    colorGray = "#8B8383";
+    colorOrange = "#f47933";
+    logoPath = "assets/image/logo-2.png";  
+    profileImage = "assets/image/profile.png";
+    displayName = "ธนาธร จึงรุ่งเรืองกิจ";
+    position = "หัวหน้าพรรค";
+    ssType = "บัญชีรายชื่อ";
+    insideContactTel = "245-8";
+    insideContactMail = "thanathorn.eak@fwp.org";
 }
